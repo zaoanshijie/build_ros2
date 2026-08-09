01294ab4ab751946fcce69d61109205949be3738
pve@ltsc.com <pve@ltsc.com>
2026-08-09 20:24:28 +0800

    update:编译缺失依赖

---
+++ script/linux/build_jazzy_dep_lib.sh
@@ -0,0 +1,59 @@
+#!/bin/bash -e
+
+#脚本的运行目录
+script_dir=$(
+    cd $(dirname $0)
+    pwd
+)
+
+work_dir="${script_dir}/dep_lib"
+# 安装目录
+install="${script_dir}/install"
+
+# 安装依赖
+apt update 
+# apt install --reinstall -y autoconf automake libtool pkg-config flex bison pkg-config m4 libxml2-dev libpopt-dev liburcu-dev
+
+# apt install liblttng-ust-python-agent1t64 liblttng-ust1t64 lttng-tools libgl1 libopengl0
+echo "编译assimp"
+cd ${work_dir}
+assimp_version="v5.3.1"
+assimp_build="${script_dir}/assimp_build"
+assimp_src="${script_dir}/assimp"
+git clone https://github.com/assimp/assimp.git --branch ${assimp_version}
+rm -rf ${assimp_build} && mkdir -p ${assimp_build}
+
+cmake -S "${assimp_src}" -B "${assimp_build}" \
+        -DBUILD_SHARED_LIBS=ON \
+        -DASSIMP_BUILD_TESTS=OFF \
+        -DASSIMP_BUILD_DOCS=OFF \
+        -DASSIMP_INSTALL=ON \
+        -DASSIMP_BUILD_ZLIB=ON \
+        -DASSIMP_BUILD_DRACO=ON  \
+        -DASSIMP_BUILD_MINIZIP=ON  \
+        -DASSIMP_HUNTER_ENABLED=OFF \
+        -DCMAKE_INSTALL_PREFIX=${install} \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+        -DCMAKE_CXX_FLAGS="-Wno-error=array-bounds" \
+        -DCMAKE_TOOLCHAIN_FILE=${script_dir}/toolchain.cmake \
+        ..
+cmake --build "${assimp_build}" --config Release -j8
+cmake --install "${assimp_build}"
+
+
+
+echo "编译console_bridge"
+cd ${work_dir}
+console_bridge_version="1.0.1"
+console_bridge_build="${script_dir}/console_bridge_build"
+console_bridge_src="${script_dir}/console_bridge"
+git clone https://github.com/ros/console_bridge.git --branch ${console_bridge_version}
+
+cmake -S "${console_bridge_src}" -B "${console_bridge_build}" \
+      -DCMAKE_INSTALL_PREFIX=${install} \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_TOOLCHAIN_FILE=${script_dir}/toolchain.cmake \
+      ..
+cmake --build "${console_bridge_build}" --config Release -j8
+cmake --install "${console_bridge_build}"
--- script/linux/run_build.sh
+++ script/linux/run_build.sh
@@ -23,6 +23,8 @@ function print_info() {
   whoami
   echo "输出目录信息"
   ls -alh
+  echo "输出python版本"
+  python --version
 }
 function print_help() {
   echo "-r ros2的版本 默认jazzy"
@@ -101,6 +103,7 @@ else
   docker exec ${docker_name} /bin/bash -c "echo '容器运行成功,开始执行${build_target}编译'"
   docker exec ${docker_name} /bin/bash -c "cp -r /workspace/* ${docker_ros2_dir}"
   docker exec ${docker_name} /bin/bash -c "cd ${docker_ros2_dir} && /bin/bash build.sh -r ${ros2_version} -t ${build_target}"
+  docker exec ${docker_name} /bin/bash -c "cd ${docker_ros2_dir} && /bin/bash build_${ros2_version}_dep_lib.sh"
   docker exec ${docker_name} /bin/bash -c "cd ${docker_ros2_dir} && tar -cavf ros2.tar.bz2 install && mv ros2.tar.bz2 /workspace"
   docker exec ${docker_name} /bin/bash -c "cd ${docker_ros2_dir} && cp dep_data.txt /workspace"
   docker exec ${docker_name} /bin/bash -c "cd ${docker_ros2_dir} && cp deps_packages.tar.bz2 /workspace"
